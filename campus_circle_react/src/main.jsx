4b4594aeed82df32848ec968596bfbe0ae730ba7
ArchilGajera <ArchilGajera203@gmail.com>
2025-04-15 22:15:29 +0530

    Refactor main.jsx to uncomment App component and comment out SignUp import

--- campus_circle_react/src/main.jsx
+++ campus_circle_react/src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-import SignUp from './pages/SignUp'
+// import SignUp from './pages/SignUp'
 import BookingForm from './components/Bokingform'
 // import Signin from './pages/Signin'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div>
-            {/* <App/> */}
-            <SignUp/>
+            <App/>
+            {/* <SignUp/> */}
                      
       
      </div>      
