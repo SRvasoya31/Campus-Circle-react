8f8e5d74079c475396fd15554a759295948861d7
SRvasoya5 <srvasoya5@gmail.com>
2025-04-22 00:00:24 +0530

    refactor main.jsx to remove unused imports and clean up code

--- campus_circle_react/src/main.jsx
+++ campus_circle_react/src/main.jsx
@@ -2,9 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-import SignUp from './pages/SignUp'
-import BookingForm from './components/Bokingform'
-// import Signin from './pages/Signin'
+
+
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
